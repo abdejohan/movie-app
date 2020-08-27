e3354b8b4141a5eeb8325caf92946f9b21dce6b1
Johan Abde <abdejohan@gmail.com>
2020-08-28 00:42:46 +0200

    Add backendService and messageService Add basic HTML structure for nav bar Add Routes to components from nav bar

--- src/app/actor/actor.component.ts
+++ src/app/actor/actor.component.ts
@@ -1,15 +1,33 @@
 import { Component, OnInit } from '@angular/core';
+import { BackendService } from '../backend.service';
+import { MessageService } from '../message.service';
 
 @Component({
-  selector: 'app-actor',
-  templateUrl: './actor.component.html',
-  styleUrls: ['./actor.component.css']
+  selector: 'app-actorsearch',
+  templateUrl: './actorsearch.component.html',
+  styleUrls: ['./actorsearch.component.css']
 })
-export class ActorComponent implements OnInit {
 
-  constructor() { }
+export class ActorsearchComponent implements OnInit {
+  person: any;
+  searchInput: any;
+
+  constructor(
+    private backendService: BackendService,
+    private messageService: MessageService
+    ) { }
 
   ngOnInit(): void {
+  }
+
+
+  searchActor(searchInput: string): void {
+    if (searchInput) {
+      searchInput = searchInput.replace(' ', '+');
+      this.backendService.searchActor(searchInput).subscribe(person => this.person = person);
+    } else {
+      this.messageService.add('Oh no, seems like there was an issue with the input. please try again');
+    }
   }
 
 }
