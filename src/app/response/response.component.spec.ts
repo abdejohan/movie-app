2ef0e0fba10093fe0dce4933d9f062ca20152efc
Johan Abde <abdejohan@gmail.com>
2021-01-26 22:57:28 +0100

    Final commit for updating angular

--- src/app/response/response.component.spec.ts
+++ src/app/response/response.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { ResponseComponent } from './response.component';
 
@@ -6,7 +6,7 @@ describe('ResponseComponent', () => {
   let component: ResponseComponent;
   let fixture: ComponentFixture<ResponseComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ ResponseComponent ]
     })
