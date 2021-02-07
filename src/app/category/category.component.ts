9c13a4392d34a2c4a0fe49e3d061652161daaea4
Johan Abde <abdejohan@gmail.com>
2021-02-07 14:32:54 +0100

    Style frontpage:   - Fix responsive issues with header text on mobile   - Change behavior of category container when on mobile   - Change some text from swedish to english

--- src/app/category/category.component.ts
+++ src/app/category/category.component.ts
@@ -1,14 +1,12 @@
-import { Component, OnInit } from '@angular/core';
-import { BackendService } from '../backend.service';
-import { HttpClient } from '@angular/common/http';
-import { Router, ActivatedRoute, ParamMap } from '@angular/router';
-
-
+import { Component, OnInit } from "@angular/core";
+import { BackendService } from "../backend.service";
+import { HttpClient } from "@angular/common/http";
+import { Router, ActivatedRoute, ParamMap } from "@angular/router";
 
 @Component({
-  selector: 'app-category',
-  templateUrl: './category.component.html',
-  styleUrls: ['./category.component.css']
+  selector: "app-category",
+  templateUrl: "./category.component.html",
+  styleUrls: ["./category.component.css"],
 })
 export class CategoryComponent implements OnInit {
   public movieCategory: any;
@@ -18,30 +16,31 @@ export class CategoryComponent implements OnInit {
     private httpclient: HttpClient,
     private route: ActivatedRoute,
     private backendservice: BackendService
-  ) { }
+  ) {}
 
   ngOnInit(): void {
     this.fetchApiData();
   }
 
   fetchApiData() {
-    this.route.params.subscribe(params => { this.path = params.category; });
-    if (this.path === 'top-rated') {
-      this.path = "Top 20 Highest Rated Movies:";
-      this.backendservice.getTopRated().subscribe(res => this.movieCategory = res);
-    } else if (this.path === 'upcoming') {
-      this.path = "Top 20 Upcoming Movies:";
-      this.backendservice.getUpcoming().subscribe(res => this.movieCategory = res);
-    } else if (this.path === 'popular' || !this.path) {
-      this.path = "Top 20 Most Popular Movies:";
-      this.backendservice.getPopular().subscribe(res => this.movieCategory = res);
-    } 
+    this.route.params.subscribe((params) => {
+      this.path = params.category;
+    });
+    if (this.path === "top-rated") {
+      this.path = "Top 20 Highest Rated Movies";
+      this.backendservice
+        .getTopRated()
+        .subscribe((res) => (this.movieCategory = res));
+    } else if (this.path === "upcoming") {
+      this.path = "Top 20 Upcoming Movies";
+      this.backendservice
+        .getUpcoming()
+        .subscribe((res) => (this.movieCategory = res));
+    } else if (this.path === "popular" || !this.path) {
+      this.path = "Top 20 Most Popular Movies";
+      this.backendservice
+        .getPopular()
+        .subscribe((res) => (this.movieCategory = res));
+    }
   }
-
-
-
-
-
-
-
 }
